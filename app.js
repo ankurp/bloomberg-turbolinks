eb9c8fe64a141ba6593f6cb235f91d6b8e15a25a
Ankur Patel <ankur.patel@ymail.com>
2016-11-06 17:13:50 -0500

    Init

---
+++ app.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const app = express();
+const request = require('request');
+const cheerio = require('cheerio');
+
+/**
+ * Middleware
+ */
+app.use(express.static('public'));
+app.use(cookieParser());
+
+/**
+ * Routes
+ */
+app.get('*', function (req, res) {
+  request({
+    url: `http://www.bloomberg.com${req.path}`,
+  }, (error, response, body) => {
+    if (error) {
+      return res.send(error);
+    }
+
+    let $ = cheerio.load(body);
+    $('<script src="/turbolinks.js"></script>').appendTo('head');
+    $('a').each((index, link) => {
+      const href = link.attribs.href;
+      if (href) {
+        link.attribs.href = href.replace(/http:\/\/www\.bloomberg\.com\//, '/');  
+      }
+    });
+    $('script[src="//cdn.optimizely.com/js/4368606971.js"], link[href^="https://nav.bloomberg.com"], link[href^="https://www.bbthat.com"], .facemelter-container, .bb-nav-placeholder, nav').remove();
+    $('base').attr('href', 'http://gadfly.bloomberg.com:9292');
+    res.send($.html());
+  });
+});
+
+/**
+ * Start server
+ */
+let server = app.listen(process.env.PORT || 9292, function () {
+  let host = server.address().address;
+  let port = server.address().port;
+
+  if (host === '::') {
+    host = 'localhost';
+  }
+
+  console.log('Node.js app listening at http://%s:%s', host, port);
+});
